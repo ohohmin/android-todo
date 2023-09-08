b0193e91d052c0dd99f4766728ac94f8990d3927
이규호 <dlrbgh@ideadesignlab.kr>
2023-09-09 07:31:19 +0900

    데이터바인딩

--- app/src/main/java/com/example/todo/MainActivity.kt
+++ app/src/main/java/com/example/todo/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.todo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.todo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // binding 변수 선언
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // binding 변수에 layout 을 연결
+        binding = DataBindingUtil
+            .setContentView(this, R.layout.activity_main)
+
+        binding.title // ...
+
     }
 }
